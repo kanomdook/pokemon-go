cc46ed3fd93253dedbc7f4ec5b080da4864528f8
kanomdook <kanung.2535@gmail.com>
2018-06-22 16:04:03 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,37 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { CameraPreview, CameraPreviewOptions } from '@ionic-native/camera-preview';
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, cameraPreview: CameraPreview) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      const cameraPreviewOpts: CameraPreviewOptions = {
+        x: 0,
+        y: 0,
+        width: window.screen.width,
+        height: window.screen.height,
+        camera: 'front',
+        tapPhoto: false,
+        previewDrag: false,
+        toBack: true,
+        alpha: 1
+      };
+
+      cameraPreview.startCamera(cameraPreviewOpts).then(
+        (res) => {
+          
+        },
+        (err) => {
+          alert(err);
+        });
     });
   }
 }
